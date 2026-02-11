69207783e29f056e9f9d95758151868b97fa8ebd
Happy_Diogenes <tibu10@naver.com>
2026-02-11 18:31:48 +0900

    0211 수
    첫 swift project 생성.

---
+++ detail_timer/detail_timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  detail_timer
+//
+//  Created by Soo on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ detail_timer/detail_timer/detail_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  detail_timerApp.swift
+//  detail_timer
+//
+//  Created by Soo on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct detail_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
